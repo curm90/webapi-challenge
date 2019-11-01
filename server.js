d41815ff87642d52a3dcb61aa03e879f3e57cc08
Liam Sutton <devliam90@gmail.com>
2019-11-01 08:22:37 +0000

    add actions endpoints

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const helmet = require('helmet');
 
 const server = express();
 
+const projectsRouter = require('./data/routers/projectsRouter');
+const actionsRouter = require('./data/routers/actionsRouter');
+
 server.use(helmet());
 server.use(logger);
 server.use(express.json());
@@ -10,6 +13,9 @@ server.use(express.json());
 server.get('/', (req, res) => {
   res.send('server is running');
 });
+
+server.use('/projects', projectsRouter);
+server.use('/actions', actionsRouter);
 
 function logger(req, res, next) {
   const { method, originalUrl } = req;
